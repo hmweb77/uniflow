6e5594d003d589167576052b197652136717006b
hmweb77 <oussamahamdani1@gmail.com>
2026-02-16 17:07:41 +0000

    adding google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,12 @@
 // src/app/layout.js
 
+import Script from 'next/script';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/contexts/ThemeContext';
 import { LocaleProvider } from '@/contexts/LocaleContext';
+
+const GA_MEASUREMENT_ID = 'G-PKX0WQ3F6E';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -42,6 +45,18 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
         style={{ backgroundColor: 'var(--bg-primary)', color: 'var(--text-primary)' }}
       >
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
         <ThemeProvider>
           <LocaleProvider>{children}</LocaleProvider>
         </ThemeProvider>
